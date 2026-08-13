3b288969fac2b765e568fc0cb31eab97c2b670c0
sathishkumar <sathish.engineer1@gmail.com>
2026-08-13 14:16:35 +0200

    build: Added the new AndroidFeatureUiConventionPlugin.kt

--- auth/presentation/build.gradle.kts
+++ auth/presentation/build.gradle.kts
@@ -1,23 +1,9 @@
 plugins {
-    alias(libs.plugins.android.library)
+    alias(libs.plugins.runtracker.android.feature.ui)
 }
 
 android {
     namespace = "com.sathish.auth.presentation"
-    compileSdk {
-        version = release(37)
-    }
-
-    defaultConfig {
-        minSdk = 24
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-
 }
 
 dependencies {
